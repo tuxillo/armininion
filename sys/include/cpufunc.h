1b8918ab626ce0fda9f0fc510dc64bc4b1691fc2
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2015-08-20 15:22:19 -0700

    Add CPU detection
    - Also add some headers with:
      Inline functions to read some registers
      Enums with Implementer IDs

--- sys/include/cpufunc.h
+++ sys/include/cpufunc.h
@@ -10,4 +10,46 @@ get_currentEL(void)
 
 	return el >> 2;
 }
+
+static __inline uint32_t
+get_SPSel(void)
+{
+	uint32_t sps;
+
+	__asm __volatile("mrs %0, SPSel" : "=r" (sps));
+
+	return sps;
+}
+
+static __inline uint32_t
+get_DAIF(void)
+{
+	uint32_t d;
+
+	__asm __volatile("mrs %0, DAIF" : "=r" (d));
+
+	return d;
+}
+
+static __inline u_int64_t
+get_MPIDR_EL1(void)
+{
+	u_int64_t mp;
+
+	__asm __volatile("mrs %0, MPIDR_EL1" : "=r" (mp));
+
+	return mp;
+}
+
+static __inline uint32_t
+get_MIDR_EL1(void)
+{
+	uint32_t idr;
+
+	__asm __volatile("mrs %0, MIDR_EL1" : "=r" (idr));
+
+	return idr;
+}
+
+
 #endif	/* !_CPU_CPUFUNC_H_ */
--- sys/kern/kern.c
+++ sys/kern/kern.c
@@ -1,9 +1,67 @@
 #include <subr.h>
+#include <cpu.h>
 #include <cpufunc.h>
 
-void kern(void)
+static const char *
+cpu_implementer(uint8_t id)
+{
+	switch(id) {
+	case CPU_VENDOR_ARM:
+		return "ARM Limited";
+	case CPU_VENDOR_BROADCOM:
+		return "Broadcom";
+	case CPU_VENDOR_CAVIUM:
+		return "Cavium";
+	case CPU_VENDOR_DEC:
+		return "Digital Equipment Corp.";
+	case CPU_VENDOR_INFINEON:
+		return "Infineon";
+	case CPU_VENDOR_MOTOROLA:
+		return "Motorola";
+	case CPU_VENDOR_NVIDIA:
+		return "nVidia";
+	case CPU_VENDOR_APCC:
+		return "Applied Micro Circuits Corp.";
+	case CPU_VENDOR_QUALCOMM:
+		return "Qualcomm";
+	case CPU_VENDOR_MARVELL:
+		return "Marvell Intl.";
+	case CPU_VENDOR_INTEL:
+		return "Intel";
+	default:
+		return "Unknown";
+	}
+}
+
+static void
+show_cpuinfo(void)
+{
+	uint8_t cpuid, cpuarch, cpuvari, cpurev;
+	uint16_t cpupart;
+	u_int64_t mp;
+	uint32_t idr;
+
+	/* Get reg values */
+	mp = get_MPIDR_EL1();
+	idr = get_MIDR_EL1();
+
+	/* Get CPU identification */
+	cpuid = (idr >> 24) & 0xff;
+	cpuvari = ((idr << 8) >> 28) & 0xf;
+	cpuarch = ((idr << 12) >> 28) & 0xf;
+	cpupart = ((idr << 16) >> 20) & 0xfff;
+	cpurev = ((idr << 28) >> 28) & 0xf;
+
+	kprintf("CPU: %s Variant=0x%x Arch=0x%x Part=0x%0x Rev=0x%x\n",
+	    cpu_implementer(cpuid),
+	    cpuvari, cpuarch, cpupart, cpurev);
+}
+
+void
+kern(void)
 {
 	kprintf("ARMininion kernel initialising...\n");
+	show_cpuinfo();
 	kprintf("Running on EL%d\n", get_currentEL());
 
 	return;
